485b5edc98e538be98f127520b3cd407f0fd08ac
Apurv Chatur <official.apurv.chatur@gmail.com>
2026-01-11 20:20:59 +0530

    XXX-004: updated brand connection for all three projects (backend, admin, frontend)

--- site/backend/src/aConnection/jBrandConnection.ts
+++ site/backend/src/aConnection/jBrandConnection.ts
@@ -1,16 +1,16 @@
 const brandConnection = {
-  aBrandID: "boilerplate",
-  bBrandName: "Boilerplate",
+  aBrandID: "aquila",
+  bBrandName: "Aquila Enterprise",
 
-  cFrontendApplicationName: "Boilerplate Frontend",
-  dAdministrationApplicationName: "Boilerplate Administration",
-  eBackendApplicationName: "Boilerplate Backend",
+  cFrontendApplicationName: "Aquila Frontend",
+  dAdministrationApplicationName: "Aquila Administration",
+  eBackendApplicationName: "Aquila Backend",
 
-  fFrontendApplicationURL: "https://boilerplate-frontend-v02.netlify.app",
-  gAdminApplicationURL: "https://boilerplate-admin-v02.netlify.app",
-  hBackendApplicationURL: "https://boilerplate-backend-v02.netlify.app",
+  fFrontendApplicationURL: "https://aquila-frontend-v02.netlify.app",
+  gAdminApplicationURL: "https://aquila-admin-v02.netlify.app",
+  hBackendApplicationURL: "https://aquila-backend-v02.netlify.app",
 
-  iEmailName: "@boilerplate.com",
+  iEmailName: "@aquila.com",
 
   jInstagramURL: "https://www.google.com/",
   kXURL: "https://www.google.com/",
@@ -24,7 +24,7 @@ const brandConnection = {
       : this.pBackendLocalURL;
   },
   pBackendLocalURL: "http://localhost:8000",
-  qBackendProductionURL: "https://boilerplate-backend-v02.onrender.com",
+  qBackendProductionURL: "https://aquila-backend-v02.onrender.com",
 
   get rFrontendBaseURL() {
     return process.env.ENVIRONMENT === "Production" ? [ 
@@ -51,10 +51,10 @@ const brandConnection = {
   sFrontendLocalURL7: "http://localhost:5179",
   sFrontendLocalURL8: "http://localhost:5180",
   sFrontendLocalURL9: "http://localhost:5181",
-  tFrontendProductionURL1: "https://boilerplate-admin-v02.netlify.app",
-  tFrontendProductionURL2: "https://boilerplate-frontend-v02.netlify.app",
+  tFrontendProductionURL1: "https://aquila-admin-v02.netlify.app",
+  tFrontendProductionURL2: "https://aquila-frontend-v02.netlify.app",
 
-  uTagName: "Skip setup. Start building.",
+  uTagName: "Elevating Success Together",
 }
 
 export default brandConnection;
